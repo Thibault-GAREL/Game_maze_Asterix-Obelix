3b700ea1a3569270dc702ec668e94af79eb3bcde
Antoine31G <antoine31.goudedranche@gmail.com>
2022-12-07 15:53:00 +0100

    code pour tableau qui bouge

---
+++ main1.c
@@ -0,0 +1,119 @@
+#include <stdio.h>
+#include "fichier.h"
+
+int main() {
+    afficherblocpermanent();
+    printf("\n");
+    int case01,case01l1,case01l2,case01l3,case01l4;
+    case01l1=case01l2=case01l3=case01l4=case01;
+    int case03,case03l1,case03l2,case03l3,case03l4;
+    case03l1=case03l2=case03l3=case03l4=case03;
+    int case05,case05l1,case05l2,case05l3,case05l4;
+    case05l1=case05l2=case05l3=case05l4=case05;
+
+
+    test(4,5,2,7,4,9,8,1,2,1,5,4,3,5,3,2,8,9,10,4,3,2,9,7,6,1,4,3,2,10);
+
+
+
+    //for (int i=0;i<20;i++){
+    //  color(i,0);
+    //printf("aaaaa");
+    //}
+    // faire un if(caseO1==0){caseO1l1=case01l2=caseO1l3=case01l4=0}
+
+
+    /*
+    piece1bout1();
+    printf("\n");
+    piece1bout2();
+    printf("\n");
+    piece1bout3();
+    printf("\n");
+    piece1bout4();
+    printf("\n");
+    printf("\n");
+    piece2bout1();
+    printf("\n");
+    piece2bout2();
+    printf("\n");
+    piece2bout3();
+    printf("\n");
+    piece2bout4();
+    printf("\n");
+    printf("\n");
+    piece3bout1();
+    printf("\n");
+    piece3bout2();
+    printf("\n");
+    piece3bout3();
+    printf("\n");
+    piece3bout4();
+
+    printf("\n");
+    printf("\n");
+    piece4bout1();
+    printf("\n");
+    piece4bout2();
+    printf("\n");
+    piece4bout3();
+    printf("\n");
+    piece4bout4();
+
+
+    printf("\n");
+    printf("\n");
+    piece5bou1();
+    printf("\n");
+    piece5bout2();
+    printf("\n");
+    piece5bout3();
+    printf("\n");
+    piece5bout4();
+    printf("\n");
+    printf("\n");
+    piece6bout1();
+    piece6bout2();
+    printf("\n");
+    printf("\n");
+    piece7bout1();
+    printf("\n");
+    piece7bout2();
+    printf("\n");
+    piece7bout3();
+    printf("\n");
+    piece7bout4();
+    printf("\n");
+    printf("\n");
+    piece8bout1();
+    printf("\n");
+    piece8bout2();
+    printf("\n");
+    piece8bout3();
+    printf("\n");
+    piece8bout4();
+    printf("\n");
+    printf("\n");
+    piece9bout1();
+    printf("\n");
+    piece9bout2();
+    printf("\n");
+    piece9bout3();
+    printf("\n");
+    piece9bout4();
+    printf("\n");
+    printf("\n");
+    piece10bout1();
+    printf("\n");
+    piece10bout2();
+    printf("\n");
+    piece10bout3();
+    printf("\n");
+    piece10bout4();*/
+
+
+
+    return 0;
+
+
+}
