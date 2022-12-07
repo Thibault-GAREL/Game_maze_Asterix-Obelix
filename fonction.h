70f874dec9f9b3288b7b30376e5f1b7c686da350
Antoine31G <antoine31.goudedranche@gmail.com>
2022-12-07 15:55:06 +0100

    code principal

---
+++ fonction.h
@@ -0,0 +1,113 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <time.h>
+#include <math.h>
+
+
+int pion[8] = {1,267,3,4,5,6,270,268};
+
+
+
+int introduction(){
+    printf("                               Bienvenue sur Labyrinthe. Une palpitante chasse au tr%csor dans un labyrithe en mouvement.\n\n",130);
+}
+
+int demandenbjoueur(){
+    printf("Combien de joueur %ctes vous ? (Max 4)\n",136);
+}
+
+
+
+int choixnom(int i){
+    printf("\n                                                               Joueur %d choisissez un nom:",i);
+}
+
+int nomjoueurs(int nom) {
+    sprintf(nom, "%s \4", nom);
+}
+
+int choixdupion(char queljoueur[100],int pion1,int pion2,int pion3) {
+    int choixpion = 0;
+    printf("\n\n%s choisissez votre pion.\n", queljoueur);
+    scanf("%d", &choixpion);
+    if (choixpion < 1 || choixpion > 8 || choixpion==pion1 || choixpion==pion2 || choixpion==pion3) {
+        printf("S%clectionnez un chiffre entre 1 et 8.\n", 130);
+        scanf("%d",&choixpion);
+        while (choixpion < 1 || choixpion > 8 || choixpion==pion1 || choixpion==pion2 || choixpion==pion3) {
+            printf("%s choisissez un nouveau pion.\n", queljoueur);
+            scanf("%d", &choixpion);
+        }
+    }
+    return choixpion;
+}
+int ecriturenom(char nom1[100],char nom2[100],char nom3[100],char nom4[100],int nbjoueur){
+    if (nbjoueur>1){
+        printf("                                                            Joueur 1 : %s\n",nom1);
+        printf("                                                            Joueur 2 : %s\n",nom2);
+    }
+    if (nbjoueur>2){
+        printf("                                                            Joueur 3 : %s\n",nom3);
+    }
+    if (nbjoueur>3){
+        printf("                                                            Joueur 4 : %s\n",nom4);
+    }
+    printf("\n\n                                                            Pour d%cterminer qui commence, faisons un tirage au sort.\n",130);
+}
+
+int tirageausort(int nbjoueur){
+    int b=0;
+    if (nbjoueur==2){
+        srand(time(NULL));
+        int a=rand()%2+1;
+        b=a;
+    }
+    if (nbjoueur==3){
+        srand(time(NULL));
+        int a=rand()%3+1;
+        b=a;
+    }
+    if (nbjoueur==4){
+        srand(time(NULL));
+        int a=rand()%4+1;
+        b=a;
+    }
+    printf("\n                                                                              C'est le Joueur %d qui commence.\n",b);
+    return b;
+}
+
+int testquijoue(int nbjoueur,int numerojoueur,int compteur,int compteur4,int compteur3){
+    if(nbjoueur<numerojoueur){
+        compteur3=0;
+        compteur4=0;
+    }
+    if (nbjoueur>=numerojoueur){
+        compteur4=1;
+    }
+    else {
+        compteur4=1;
+    }
+    return compteur4;
+}
+
+int toursuivant(int compteur1,int compteur2){
+    if (compteur1==0){
+        compteur2=1;
+    }
+    return compteur2;
+}
+
+
+int actionafaire(){
+    printf("");
+
+}
+
+//
+// Created by Antoine on 01/12/2022.
+//
+
+#ifndef PROJET_FONCTION_H
+#define PROJET_FONCTION_H
+
+#endif //PROJET_FONCTION_H
---
+++ main.c
@@ -0,0 +1,214 @@
+#include <stdio.h>
+#include "fonction.h"
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <math.h>
+
+
+int main() {
+    char nomJ1[100],nomJ2[100],nomJ3[100],nomJ4[100];
+    int nbjoueur=0;
+    int pion1;
+    int pion2;
+    int pion3;
+    int pion4;
+    int compteur1=0;
+    int compteur2=0;
+    int compteur3=0;
+    int compteur4=0;
+    int a=0;
+    int partie=1;
+    introduction();
+    demandenbjoueur();
+    scanf("%d",&nbjoueur);
+    while (nbjoueur<2 || nbjoueur>4){
+        demandenbjoueur();
+        scanf("%d",&nbjoueur);
+    }
+    for (int i=1;i<nbjoueur+1;i++){
+        if(i==1){
+            choixnom(i);
+            scanf("%s",&nomJ1);
+        }
+        else if(i==2){
+            choixnom(i);
+            scanf("%s",&nomJ2);
+            while (strcmp(nomJ2,nomJ1)==0){
+                printf("\nImpossible de choisir le m%cme nom que %s.\n",136,nomJ1);
+                choixnom(i);
+                scanf("%s",&nomJ2);
+            }
+        }
+        else if(i==3){
+            choixnom(i);
+            scanf("%s",&nomJ3);
+            while ((strcmp(nomJ3,nomJ1)==0)||(strcmp(nomJ3,nomJ2)==0)){
+                printf("\nImpossible de choisir le m%cme nom que %s ou %s.\n",136,nomJ1,nomJ2);
+                choixnom(i);
+                scanf("%s",&nomJ3);
+            }
+        }
+        else{
+            choixnom(i);
+            scanf("%s",&nomJ4);
+            while ((strcmp(nomJ4,nomJ1)==0)||(strcmp(nomJ4,nomJ2)==0)||(strcmp(nomJ4,nomJ3)==0)){
+                printf("\nImpossible de choisir le m%cme nom que %s, %s ou %s.\n",136,nomJ1,nomJ2,nomJ3);
+                choixnom(i);
+                scanf("%s",&nomJ4);
+            }
+        }
+    }
+    printf("\n\nIl y a 8 pions disponibles:\n");
+    for (int i = 0; i < 8; ++i) {
+        printf("Pion %d : %c\n",i+1,pion[i]);
+    }
+    if (nbjoueur>1){
+        pion1=choixdupion(nomJ1,0,0,0);
+        pion2=choixdupion(nomJ2,pion1,0,0);
+        if(pion1==1) {
+            sprintf(nomJ1, "%s \1", nomJ1);
+        }
+        else if (pion1==2){
+            sprintf(nomJ1, "%s \14", nomJ1);
+        }
+        else if(pion1==3){
+            sprintf(nomJ1, "%s \3", nomJ1);
+        }
+        else if (pion1==4){
+            sprintf(nomJ1, "%s \4", nomJ1);
+        }
+        else if (pion1==5){
+            sprintf(nomJ1, "%s \5", nomJ1);
+        }
+        else if(pion1==6){
+            sprintf(nomJ1, "%s \6", nomJ1);
+        }
+        else if(pion1==7){
+            sprintf(nomJ1, "%s \16", nomJ1);
+        }
+        else if(pion1==8){
+            sprintf(nomJ1, "%s \13", nomJ1);
+        }
+        if(pion2==1) {
+            sprintf(nomJ2, "%s \1", nomJ2);
+        }
+        else if (pion2==2){
+            sprintf(nomJ2, "%s \14", nomJ2);
+        }
+        else if(pion2==3){
+            sprintf(nomJ2, "%s \3", nomJ2);
+        }
+        else if (pion2==4){
+            sprintf(nomJ2, "%s \4", nomJ2);
+        }
+        else if (pion2==5){
+            sprintf(nomJ2, "%s \5", nomJ2);
+        }
+        else if(pion2==6){
+            sprintf(nomJ2, "%s \6", nomJ2);
+        }
+        else if(pion2==7){
+            sprintf(nomJ2, "%s \16", nomJ2);
+        }
+        else if(pion2==8){
+            sprintf(nomJ2, "%s \13", nomJ2);
+        }
+    }
+    if (nbjoueur>2){
+        pion3=choixdupion(nomJ3,pion1,pion2,0);
+        if(pion3==1) {
+            sprintf(nomJ3, "%s \1", nomJ3);
+        }
+        else if (pion3==2){
+            sprintf(nomJ3, "%s \14", nomJ3);
+        }
+        else if(pion3==3){
+            sprintf(nomJ3, "%s \3", nomJ3);
+        }
+        else if (pion3==4){
+            sprintf(nomJ3, "%s \4", nomJ3);
+        }
+        else if (pion3==5){
+            sprintf(nomJ3, "%s \5", nomJ3);
+        }
+        else if(pion3==6){
+            sprintf(nomJ3, "%s \6", nomJ3);
+        }
+        else if(pion3==7){
+            sprintf(nomJ3, "%s \16", nomJ3);
+        }
+        else if(pion3==8){
+            sprintf(nomJ3, "%s \13", nomJ3);
+        }
+    }
+    if (nbjoueur>3){
+        pion4=choixdupion(nomJ4,pion1,pion2,pion3);
+        if(pion4==1) {
+            sprintf(nomJ4, "%s \1", nomJ4);
+        }
+        else if (pion4==2){
+            sprintf(nomJ4, "%s \14", nomJ4);
+        }
+        else if(pion4==3){
+            sprintf(nomJ4, "%s \3", nomJ4);
+        }
+        else if (pion4==4){
+            sprintf(nomJ4, "%s \4", nomJ4);
+        }
+        else if (pion4==5){
+            sprintf(nomJ4, "%s \5", nomJ4);
+        }
+        else if(pion4==6){
+            sprintf(nomJ4, "%s \6", nomJ4);
+        }
+        else if(pion4==7){
+            sprintf(nomJ4, "%s \16", nomJ4);
+        }
+        else if(pion4==8){
+            sprintf(nomJ4, "%s \13", nomJ4);
+        }
+    }
+    ecriturenom(nomJ1,nomJ2,nomJ3,nomJ4,nbjoueur);
+    a=tirageausort(nbjoueur);
+    while (partie==1){
+        if (a==1){
+            compteur1=1;
+        }
+        else if (a==2){
+            compteur2=1;
+        }
+        else if (a==3){
+            compteur3=1;
+        }
+        else if (a==4){
+            compteur4=1;
+        }
+        if (compteur1==1){
+            compteur1=0;
+            printf("\n                                                                              C'est au tour de %s de jouer.\n",nomJ1);
+            compteur2=1;
+        }
+        compteur2= toursuivant(compteur1,compteur2);
+        if(compteur2==1){
+            compteur2=0;
+            printf("\n                                                                              C'est au tour de %s de jouer.\n",nomJ2);
+            compteur3=1;
+        }
+        compteur3= toursuivant(compteur2,compteur3);
+        compteur1=testquijoue(nbjoueur,3,compteur1,compteur4,compteur3);
+        if(compteur3==1 && nbjoueur>2){
+            compteur3=0;
+            printf("\n                                                                              C'est au tour de %s de jouer.\n",nomJ3);
+        }
+        compteur4= toursuivant(compteur3,compteur4);
+        compteur1=testquijoue(nbjoueur,4,compteur1,compteur4,compteur4);
+        if(compteur4==1 && nbjoueur==4){
+            compteur4=0;
+            printf("\n                                                                              C'est au tour de %s de jouer.\n",nomJ4);
+            compteur1=1;
+        }
+        compteur1= toursuivant(compteur4,compteur1);
+    }
+    return 0;
+}
