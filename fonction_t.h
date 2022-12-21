1954ce46ee7cbc29d10119b600dd384415d4bfce
Thb1t <thibault.garel@edu.ece.fr>
2022-12-21 17:13:03 +0100

    plateau fait sans bool et avec flèche

--- fonction_t.h
+++ fonction_t.h
@@ -18,4 +18,6 @@ void random_sans_remise(int tab_index[50]);
 
 void initalisation_affichage_plateau(int tab_index[50], GC_SPRITE tab_plateau[7][7]);
 
+void boutton ();
+
 #endif //PROJET_FONCTION_T_H
