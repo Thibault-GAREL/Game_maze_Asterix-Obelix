25882f1bb6d29160f72c0f952fe3e327c5eb7daa
RobinKoenig69 <robin.koenig@edu.ece.fr>
2023-01-06 15:39:46 +0100

    Ajout de l'image d'arrière plan le fichier Path.h

--- Path.h
+++ Path.h
@@ -1,8 +1,9 @@
 #ifndef GLOBAL_PATH
 #define GLOBAL_PATH
 
-#define FILE_ACCESS ""
+#define FILE_ACCESS "."
 #define BUTTON_PATH FILE_ACCESS ".\\Import\\Fleche_1.PNG"
+#define MENU_BACKGROUND FILE_ACCESS ".\\Import\\MENU_BACKGROUND.JPG"
 #define FONT_PATH FILE_ACCESS ".\\Import\\BruceForeverRegular.ttf"
 
 
