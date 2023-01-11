b7bd27c7cfdc6bcbcc86c039a93c1e5ecde6aed3
MatthieuGROS <matthieu.gros@edu.ece.fr>
2023-01-11 13:49:55 +0100

    r

--- main_t.c
+++ main_t.c
@@ -1,5 +1,6 @@
 #include "fonction_t.h"
 #include "Party.h"
+
 // gcc -g -Wall main_t.c Gc.c fonction_t.c Menu.c Plateau.c Player.c Part.c -lallegro -lallegro_image -lallegro_font -oProject; .\Project.exe
 //gcc -g -Wall main_t.c Gc.c fonction_t.c Menu.c Plateau.c Player.c Part.c utils.c Party.c -lallegro -lallegro_image -lallegro_font -lallegro_ttf -oProject
 
@@ -18,7 +19,7 @@ int main() {
     TextMenu textMenu;
     MenuInit(&textMenu);
 
-    while (SelectMenu==0) {
+    /*while (SelectMenu==0) {
         Update_Event(&manager);
          
         MenuDisplay(&textMenu);
@@ -39,7 +40,7 @@ int main() {
         }
     }
     
-    printf("\nSelMenu=%d", SelectMenu);    
+    printf("\nSelMenu=%d", SelectMenu); */   
     
     PARTY party;
     Party_Init(&party, 4, &manager);
